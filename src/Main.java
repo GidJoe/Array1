daa6fe035aaf60a6bda9252028ca926fad2c2bbc
GidJoe <winterm@bfwob.de>
2023-01-06 07:44:50 +0100

    Aufgabe 1 - a&b Aufgabe 2

--- src/Main.java
+++ src/Main.java
@@ -7,39 +7,52 @@ public class Main {
 
         Scanner myscan = new Scanner(System.in);
 
-        System.out.println("Wie groß soll das Array sein?");
-        int a = myscan.nextInt();
-        int[] array1 = new int[a];
-
-
-        for (int i = 0; i < a; i++) {
-            System.out.println("Bitte die " + (i + 1) + ". Zahl eingeben");
-            array1[i] = myscan.nextInt();
-        }
-
+//        System.out.println("Wie groß soll das Array sein?");
+//        int a = myscan.nextInt();
+//        int[] array1 = new int[a];
+//
+//
+//        for (int i = 0; i < a; i++) {
+//            System.out.println("Bitte die " + (i + 1) + ". Zahl eingeben");
+//            array1[i] = myscan.nextInt();
+//        }
+        int[] array1 = {7, 2, 3, 5, 6, 8, 10};
         int[] array2 = new int[array1.length];
 
-        System.out.println("Array 1 vorher" + Arrays.toString(array1));
-        System.out.println("Array 2 vorher " + Arrays.toString(array2));
+//        System.out.println("Array 1 vorher" + Arrays.toString(array1));
+//        System.out.println("Array 2 vorher " + Arrays.toString(array2));
 
         for (int z = 0, b = 1; z < array1.length; z++, b++) {
             array2[array2.length - b] = array1[z];
         }
+        int[] array3 = new int[7];
 
-        System.out.println("Array 1 nachher" + Arrays.toString(array1));
-        System.out.println("Array 2 nachher " + Arrays.toString(array2));
-
-        for (int c = 0, v = 1; c < array1.length/2; c++, v++){
-        if (array1[c]%2==0) {
-            array2[c] = array1[c];
-        } else {
-            array2[array2.length-v] = array1[c];
-
-        }}
-
-        System.out.println("Array 1 nachher" + Arrays.toString(array1));
-        System.out.println("Array 2 nachher " + Arrays.toString(array2));
-
-
+//        System.out.println("Array 1 nachher" + Arrays.toString(array1));
+//        System.out.println("Array 2 nachher " + Arrays.toString(array2));
+        //  {7, 2, 3, 5, 6, 8, 10};
+        boolean vollständig = true;
+        int i = 0, b = 1;
+        while (vollständig) {
+            if (array1[i] % 2 == 0) {
+                array3[i] = array1[i];
+                System.out.println(Arrays.toString(array3));
+                i++;
+                if (array2.length==array3.length) {
+                    vollständig = false;
+                }
+            } else {
+                array3[i] = array1[array1.length-b];
+                System.out.println(Arrays.toString(array3));
+                b++;
+                if (array2.length==array3.length) {
+                    vollständig = false;
+                }
+            }
+        }
+        System.out.println("Array 1 nachher"+Arrays.toString(array1));
+        System.out.println("Array 2 nachher "+Arrays.toString(array3));
     }
 }
+
+
+
